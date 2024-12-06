f30d7206a10cd0e6bb91d6a0e18daa725e27e61a
reljin <milan.reljin02@gmail.com>
2024-12-06 02:13:46 +0100

    test komunikacija izmedju klijenta i loadbalancer

--- Client/Client.cpp
+++ Client/Client.cpp
@@ -1,20 +1,55 @@
-// Client.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+﻿#include <iostream>
+#include <string>
+#include <winsock2.h> // Windows socket API
+#include <ws2tcpip.h> // Za inet_pton()
 
-#include <iostream>
+#pragma comment(lib, "ws2_32.lib")
+
+#define SERVER_IP_ADDRESS "127.0.0.1"
+#define SERVER_PORT 5059
+#define BUFFER_SIZE 256
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    WSADATA wsaData;
+    if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
+        std::cerr << "Greska pri inicijalizaciji Winsock-a. Kod greske: " << WSAGetLastError() << std::endl;
+        return 1;
+    }
+
+    SOCKET clientSocket = socket(AF_INET, SOCK_STREAM, 0);
+    if (clientSocket == INVALID_SOCKET) {
+        std::cerr << "Greska pri kreiranju soketa. Kod greske: " << WSAGetLastError() << std::endl;
+        WSACleanup();
+        return 1;
+    }
+
+    sockaddr_in serverAddress;
+    serverAddress.sin_family = AF_INET; // IPv4 protokol
+    inet_pton(AF_INET, SERVER_IP_ADDRESS, &serverAddress.sin_addr); // Moderni način za konverziju IP adrese
+    serverAddress.sin_port = htons(SERVER_PORT);
+
+    if (connect(clientSocket, (sockaddr*)&serverAddress, sizeof(serverAddress)) == SOCKET_ERROR) {
+        std::cerr << "Neuspesno povezivanje na server. Kod greske: " << WSAGetLastError() << std::endl;
+        closesocket(clientSocket);
+        WSACleanup();
+        return 1;
+    }
+
+    std::string data;
+    std::cout << "Unesite poruku za server: ";
+    std::getline(std::cin, data);
+
+    if (send(clientSocket, data.c_str(), data.size(), 0) == SOCKET_ERROR) {
+        std::cerr << "Greska pri slanju podataka. Kod greske: " << WSAGetLastError() << std::endl;
+        closesocket(clientSocket);
+        WSACleanup();
+        return 1;
+    }
+
+    std::cout << "Podaci poslati serveru!" << std::endl;
+
+    closesocket(clientSocket);
+    WSACleanup();
+    return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
--- LoadBalancer/LoadBalancer.cpp
+++ LoadBalancer/LoadBalancer.cpp
@@ -1,20 +1,77 @@
-// LoadBalancer.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+﻿#include <iostream>
+#include <winsock2.h>
+#include <thread>
+#include <vector>
+#include <ws2tcpip.h> // Za InetPton()
 
-#include <iostream>
+#pragma comment(lib, "ws2_32.lib")
 
-int main()
-{
-    std::cout << "Hello World!\n";
+
+
+#define SERVER_IP_ADDRESS "127.0.0.1"
+#define SERVER_PORT 5059
+#define BUFFER_SIZE 256
+
+void handleClient(SOCKET clientSocket) {
+    char buffer[BUFFER_SIZE];
+    int bytesReceived;
+
+    bytesReceived = recv(clientSocket, buffer, sizeof(buffer), 0);
+    if (bytesReceived > 0) {
+        std::string receivedData(buffer, bytesReceived);
+        std::cout << "Primljen zahtev od klijenta: " << receivedData << std::endl;
+        send(clientSocket, buffer, bytesReceived, 0);
+    }
+
+    closesocket(clientSocket);
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+int main() {
+    WSADATA wsaData;
+    if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
+        std::cerr << "Greška pri inicijalizaciji Winsock-a. Kod greške: " << WSAGetLastError() << std::endl;
+        return 1;
+    }
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+    SOCKET listenSocket = socket(AF_INET, SOCK_STREAM, 0);
+    if (listenSocket == INVALID_SOCKET) {
+        std::cerr << "Greška pri kreiranju soketa za slušanje." << std::endl;
+        WSACleanup();
+        return 1;
+    }
+
+    sockaddr_in serverAddr;
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_port = htons(SERVER_PORT);
+    inet_pton(AF_INET, SERVER_IP_ADDRESS, &serverAddr.sin_addr);
+
+    if (bind(listenSocket, (struct sockaddr*)&serverAddr, sizeof(serverAddr)) == SOCKET_ERROR) {
+        std::cerr << "Greška pri povezivanju soketa." << std::endl;
+        closesocket(listenSocket);
+        WSACleanup();
+        return 1;
+    }
+
+    if (listen(listenSocket, SOMAXCONN) == SOCKET_ERROR) {
+        std::cerr << "Greška pri slušanju." << std::endl;
+        closesocket(listenSocket);
+        WSACleanup();
+        return 1;
+    }
+
+    std::cout << "Load balancer radi na " << SERVER_IP_ADDRESS << ":" << SERVER_PORT << "..." << std::endl;
+
+    while (true) {
+        SOCKET clientSocket = accept(listenSocket, NULL, NULL);
+        if (clientSocket == INVALID_SOCKET) {
+            std::cerr << "Greška pri prihvatanju veze." << std::endl;
+            continue;
+        }
+
+        std::thread(handleClient, clientSocket).detach();
+    }
+
+    closesocket(listenSocket);
+    WSACleanup();
+    return 0;
+}
